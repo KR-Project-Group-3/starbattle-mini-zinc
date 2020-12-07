b34989c77a471fd2d3d54ea7b2640a23ca0cb737
EricaCoppolillo <erica.coppolillo1999@gmail.com>
2020-12-07 18:29:06 +0100

    Fixing commit of solution matrix updates

--- src/star_battle/model/SolutionMatrix.java
+++ src/star_battle/model/SolutionMatrix.java
@@ -1,22 +1,98 @@
 package star_battle.model;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class SolutionMatrix {
 
-    private String solutionFilePath;
-    private boolean[][] solutionMatrix = {
-    		{false, false, true, false, false},
-    		{true, false, false, false, false},
-    		{false, false, false, false, true},
-    		{false, true, false, false, false},
-    		{false, false, false, true, false}
-    };
+    private InstanceMatrix instanceMatrix;
+    private boolean[][] solutionMatrix;
+    private String generatedInstanceFilePath;
+    private Process process;
 
-    public SolutionMatrix(String solutionFilePath){
-        this.solutionFilePath = solutionFilePath; 
+    public SolutionMatrix(InstanceMatrix instanceMatrix){
+        this.instanceMatrix = instanceMatrix;
+        this.generatedInstanceFilePath = "instance.dzn";
     }
 
-    public void parseMatrix(){
+    private void generateDataFile() throws IOException {
 
+        Charset charset = StandardCharsets.US_ASCII;
+        BufferedWriter writer = Files.newBufferedWriter(Path.of(this.generatedInstanceFilePath), charset);
+        String textToWrite = "num_stars = " + this.instanceMatrix.getStarsNumber() + ";\n";
+        textToWrite += "dim = 5;\n" + "sectors = [|\n";
+
+        for (int i = 0; i < instanceMatrix.getDimension(); ++i){
+            for (int j = 0; j < instanceMatrix.getDimension(); j++) {
+                textToWrite += this.instanceMatrix.getSector(i, j);
+
+                if(j < this.instanceMatrix.getDimension() - 1){
+                    textToWrite += ", ";
+                }
+            }
+
+            textToWrite += "|";
+
+            if(i < this.instanceMatrix.getDimension() - 1)
+                textToWrite += "\n";
+        }
+
+        textToWrite += "];";
+        writer.write(textToWrite, 0, textToWrite.length());
+        writer.close();
+    }
+
+    public void parseMatrix() throws IOException {
+
+        generateDataFile();
+        File instanceFile = new File(this.generatedInstanceFilePath);
+
+        boolean isWindows = System.getProperty("os.name")
+                .toLowerCase().startsWith("windows");
+
+        if(isWindows) {
+            this.process = Runtime.getRuntime()
+                    .exec("cmd /c \"C:\\Program Files\\MiniZinc\\minizinc\" " + this.generatedInstanceFilePath +
+                                    " models" + File.separator + "star_puzzle.mzn"
+                            , null, new File(instanceFile.getAbsoluteFile().getParent()));
+        } else {
+            this.process = Runtime.getRuntime()
+                    .exec("sh -c minizinc " + this.generatedInstanceFilePath +
+                                    " models" + File.separator + "star_puzzle.mzn"
+                            , null, new File(instanceFile.getAbsoluteFile().getParent()));
+        	
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = "";
+
+        this.solutionMatrix = new boolean[this.instanceMatrix.getDimension()][this.instanceMatrix.getDimension()];
+        for (int i = 0; (line = reader.readLine()) != null; ++i) {
+
+            if(i == 0 || i == this.instanceMatrix.getDimension() + 1)
+                continue;
+
+            String[] lineArray = line.split(" ");
+            System.out.println(line);
+            for (int j = 0; j < this.instanceMatrix.getDimension(); j++) {
+
+                if(Integer.parseInt(lineArray[j]) == 1)
+                    solutionMatrix[i - 1][j] = true;
+                else
+                    solutionMatrix[i - 1][j] = false;
+            }
+        }
+    }
+
+    public boolean isThereAStar(int row, int column){
+        return this.solutionMatrix[row][column];
     }
     
     public boolean get(int i, int j) {
